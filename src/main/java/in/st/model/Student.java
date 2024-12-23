a164ad4acaa5940966aaaf7cdbea18fa4f9f627c
nthokal24 <nthokal3@gmail.com>
2024-12-23 18:39:25 +0530

    model

---
+++ src/main/java/in/st/model/Student.java
@@ -0,0 +1,50 @@
+package in.st.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer Id;
+	private String name;
+	private String city;
+	public Integer getId() {
+		return Id;
+	}
+	public void setId(Integer id) {
+		Id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public Student(Integer id, String name, String city) {
+		super();
+		Id = id;
+		this.name = name;
+		this.city = city;
+	}
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Student [Id=" + Id + ", name=" + name + ", city=" + city + "]";
+	}
+	
+	
+	
+	
+}
