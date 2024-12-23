d248cd15c0b2b66f367763a0f203fef2c57d3681
nthokal24 <nthokal3@gmail.com>
2024-12-23 18:17:25 +0530

    First

---
+++ src/main/java/in/st/StudentMavenApplication.java
@@ -0,0 +1,13 @@
+package in.st;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentMavenApplication.class, args);
+	}
+
+}
