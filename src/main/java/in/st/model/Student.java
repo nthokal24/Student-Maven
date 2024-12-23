4ebd7cceade558417b60a4fcba9d7e12f7dc889c
nthokal24 <nthokal3@gmail.com>
2024-12-23 20:31:05 +0530

    Repo Added

--- src/main/java/in/st/model/Student.java
+++ src/main/java/in/st/model/Student.java
@@ -1,9 +1,11 @@
 package in.st.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class Student {
 
 	@Id
